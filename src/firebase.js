4a2d7920a3657395c135e8bf4454a141dd329919
VandanaPv <marlabsvandana@titan.co.in>
2020-10-05 15:34:51 +0530

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXrc-v7dAikrDN6pm65p7koS_-aY2JL0I",
+    authDomain: "whatsapp-mern-50671.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-50671.firebaseio.com",
+    projectId: "whatsapp-mern-50671",
+    storageBucket: "whatsapp-mern-50671.appspot.com",
+    messagingSenderId: "134155166891",
+    appId: "1:134155166891:web:4bb764c6cd7d916f1483ee"
+  };
